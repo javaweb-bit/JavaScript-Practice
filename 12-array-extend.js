b0b6aa3d17cb1411a1da723cc01fb9c8eef01cdc
남승균 <skyun.nam@gmail.com>
2021-04-06 15:15:43 +0900

    Array map

--- 12-array-extend.js
+++ 12-array-extend.js
@@ -37,4 +37,43 @@ function testFilter() {
     result = source.filter(item => item % 2 == 0);  //  짝수인 것만 필터링 -> 새배열로 생성
     console.log("짝수 필터링(filter):", result);
 }
-testFilter();
+// testFilter();
+
+function testMap() {
+    console.log("---------- map");
+
+    const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    //  source 배열 내부의 모든 요소를 2배 할 때
+    //  ES5 방식
+    let multiply = [];
+
+    for (let i = 0; i < source.length; i++) {
+        let item = source[i];
+        multiply.push(item * 2);
+    }
+    console.log("배열 변형(for):", multiply);
+
+    //  map 함수
+    multiply = source.map(item => item * 2);
+    console.log("배열 변형(map):", multiply);
+}
+// testMap();
+
+function testChain() {
+    const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    //  원본 배열에서 짝수만 추출해서 2배를 해 봅시다.
+    //  ES5 방식
+    let target = [];
+    for (let i = 0; i < source.length; i++) {
+        let item = source[i];
+        if (item % 2 == 0) {    //  짝수
+            target.push(item * 2);
+        }
+    }
+    console.log("target:", target);
+
+    //  filter -> map -> 결과
+    target = source.filter(item => item % 2 == 0).map(item => item * 2);
+    console.log("target:(filter -> map):", target);
+}
+testChain();
